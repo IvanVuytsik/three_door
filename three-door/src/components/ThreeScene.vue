8c22ca9fcd2bad8c4ab24fa62db1d4782eef11e2
iv_vuytsik <iv_vuytsik@controlsystems.ru>
2025-07-16 12:57:04 +0300

    commit

--- three-door/src/components/ThreeScene.vue
+++ three-door/src/components/ThreeScene.vue
@@ -5,7 +5,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { createDoor, updateDoor } from './Door'
 import { createGUI } from '../utils/gui'
 
